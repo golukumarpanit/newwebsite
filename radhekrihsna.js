c6cc4f0d22c00fb595e7c29185d72d36373da09c
unknown <you@example.com>
2025-02-27 20:09:37 +0530

    change

---
+++ radhekrihsna.js
@@ -0,0 +1,29 @@
+
+function adca2024() {
+    window.open("https://gcsm2024.netlify.app/", "_blank");
+}
+function TYPING2024() {
+    window.open("https://typing-2024.netlify.app/", "_blank");
+}
+function TYPING2023() {
+    window.open("https://gcsmtyp.netlify.app/", "_blank");
+}
+function TYPING2024() {
+    window.open("https://typing-2024.netlify.app/", "_blank");
+}function ADCA2022_2023() {
+    window.open("https://globalcomputer.netlify.app/", "_blank");
+}
+function showTab(tabId) {
+    // सभी टैब कंटेंट को छुपाएं
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    // केवल चयनित टैब को दिखाएं
+    const selectedTab = document.getElementById(tabId);
+    if (selectedTab) {
+        selectedTab.classList.add('active');
+    }
+}
+{/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2157281636056882" crossorigin="anonymous"></script> */}
